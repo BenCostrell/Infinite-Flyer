2715e7ce2ef6521d22800fb44828f500f1d875dd
Ben Costrell <bencostrell@gmail.com>
2017-03-22 20:54:00 -0400

    timescale change messages mostly there
    i kinda hate this but oh well

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,58 +6,92 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
     public GameObject ship;
-
     public GameObject timePassedUI;
+    public GameObject messageUI;
+    public float realTimePerSection;
+    public float pauseTime;
+
     private Text timePassedText;
+    public Text messageText;
 
     private int timePassed;
-    public float realTimePerSection;
     private float realTimePerTimePass;
     private float timeSinceLastTimePass;
+    public bool paused;
 
     private TimeScale currentTimeScale;
 
     // Use this for initialization
 	void Start () {
+        InitializeServices();
         timePassedText = timePassedUI.GetComponent<Text>();
+        messageText = messageUI.GetComponent<Text>();
+        messageUI.SetActive(false);
         InitializeTimeScaleObjects();
         timePassed = 0;
         timeSinceLastTimePass = 0;
         realTimePerTimePass = realTimePerSection / currentTimeScale.numUnitsUntilNextTimeScale;
+        paused = false;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (timeSinceLastTimePass >= realTimePerTimePass)
+        Services.TaskManager.Update();
+        if (!paused)
         {
-            PassTime();
-            timeSinceLastTimePass = 0;
-        }
-        else
-        {
-            timeSinceLastTimePass += Time.deltaTime;
+            if (timeSinceLastTimePass >= realTimePerTimePass)
+            {
+                PassTime();
+                timeSinceLastTimePass = 0;
+            }
+            else
+            {
+                timeSinceLastTimePass += Time.deltaTime;
+            }
         }
 	}
 
+    void InitializeServices()
+    {
+        Services.GameManager = this;
+        Services.TaskManager = new TaskManager();
+    }
+
     void InitializeTimeScaleObjects()
     {
-        TimeScale seconds = new TimeScale("seconds elapsed", 60, "");
-        TimeScale minutes = new TimeScale("minutes elapsed", 60, "");
-        TimeScale hours = new TimeScale("hours elapsed", 24, "");
-        TimeScale days = new TimeScale("days elapsed", 30, "");
-        TimeScale months = new TimeScale("months elapsed", 12, "");
-        TimeScale years = new TimeScale("years elapsed", 10, "");
-        TimeScale decades = new TimeScale("decades elapsed", 10, "");
-        TimeScale lifetimes = new TimeScale("lifetimes passed", 10, "");
-        TimeScale civilizations = new TimeScale("civilizations fallen", 10, "");
-        TimeScale recordedHumanHistories = new TimeScale("human histories recorded", 20, "");
-        TimeScale anatomicalEvolutionaryDivergences = new TimeScale("anatomical evolutionary divergences", 30, "");
-        TimeScale speciesDivergences = new TimeScale("new species evolved", 3, "");
-        TimeScale animalKingdomDivergences = new TimeScale("new animal kingdoms evolved", 3, "");
-        TimeScale lifeformDivergences = new TimeScale("new forms of life evolved", 8, "");
-        TimeScale starFormations = new TimeScale("suns formed from nothing", 3, "");
-        TimeScale universeLifetimes = new TimeScale("lifetimes of the universe", 1, "");
+        TimeScale seconds = new TimeScale("seconds elapsed", 60, 
+            "oh boy, explorin the universe");
+        TimeScale minutes = new TimeScale("minutes elapsed", 60, 
+            "can't wait to learn the secrets of reality");
+        TimeScale hours = new TimeScale("hours elapsed", 24, 
+            "check out the view");
+        TimeScale days = new TimeScale("days elapsed", 30, 
+            "got any threes?");
+        TimeScale months = new TimeScale("months elapsed", 12, 
+            "holy shit it's Mars");
+        TimeScale years = new TimeScale("years elapsed", 10, 
+            "at long last... it's pluto");
+        TimeScale decades = new TimeScale("decades elapsed", 10, 
+            "RIP, you died. your child will continue searching the cosmos");
+        TimeScale lifetimes = new TimeScale("generations passed", 30, 
+            "we honor the ancient founders of our society who began this journey");
+        TimeScale civilizations = new TimeScale("civilizations fallen", 10, 
+            "");
+        TimeScale recordedHumanHistories = new TimeScale("human histories recorded", 20, 
+            "humans on earth have evolved to the point where they are noticeably different looking");
+        TimeScale anatomicalEvolutionaryDivergences = new TimeScale("anatomical evolutionary divergences", 30, 
+            "there is no such species as human on earth anymore");
+        TimeScale speciesDivergences = new TimeScale("new species evolved", 3,
+            "difference between you and dominant earth species is like difference between mammals and reptiles");
+        TimeScale animalKingdomDivergences = new TimeScale("new animal kingdoms evolved", 3, 
+            "life on earth is almost unrecognizable");
+        TimeScale lifeformDivergences = new TimeScale("new forms of life evolved", 8, 
+            "life on earth no longer exists, the sun exploded, also the milky way collided with the andromeda galaxy");
+        TimeScale starFormations = new TimeScale("suns formed from nothing", 3, 
+            "who knows? maybe the universe has collapsed in on itself or something");
+        TimeScale universeLifetimes = new TimeScale("lifetimes of the universe", 1, 
+            "");
 
         seconds
             .Then(minutes)
@@ -77,7 +111,6 @@ public class GameManager : MonoBehaviour {
             .Then(universeLifetimes);
 
         currentTimeScale = seconds;
-        Debug.Log(currentTimeScale);
     }
 
     public void SetShipVelocity(float vel)
@@ -101,9 +134,16 @@ public class GameManager : MonoBehaviour {
     {
         if (timePassed == currentTimeScale.numUnitsUntilNextTimeScale)
         {
+            PauseForMessage();
             currentTimeScale = currentTimeScale.nextTimeScale;
             realTimePerTimePass = realTimePerSection / currentTimeScale.numUnitsUntilNextTimeScale;
             timePassed = 1;
         }
+    }
+
+    void PauseForMessage()
+    {
+        PauseForMessage pause = new PauseForMessage(currentTimeScale);
+        Services.TaskManager.AddTask(pause);
     }
 }
---
+++ Assets/Scripts/PauseForMessage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseForMessage : Task {
+    public PauseForMessage(TimeScale scale)
+    {
+        timeScale = scale;
+    }
+
+    private TimeScale timeScale;
+    private float timeElapsed;
+
+    protected override void Init()
+    {
+        Services.GameManager.messageUI.SetActive(true);
+        Services.GameManager.messageText.text = timeScale.transitionMessage;
+        timeElapsed = 0;
+        Services.GameManager.paused = true;
+    }
+
+    internal override void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= Services.GameManager.pauseTime)
+        {
+            SetStatus(TaskStatus.Success);
+        }
+    }
+
+    protected override void OnSuccess()
+    {
+        Services.GameManager.messageUI.SetActive(false);
+        Services.GameManager.paused = false;
+    }
+}
